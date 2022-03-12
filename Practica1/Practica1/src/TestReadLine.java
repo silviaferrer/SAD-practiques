f8abe5d918dbe539f204b7ba243ff57039142242
Silvia Ferrer <62883007+silviaferrer@users.noreply.github.com>
2022-03-12 14:30:59 +0100

    Update TestReadLine.java

--- Practica1/Practica1/src/TestReadLine.java
+++ Practica1/Practica1/src/TestReadLine.java
@@ -1,6 +1,5 @@
 
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -11,7 +10,7 @@ import java.util.logging.Logger;
  */
 public class TestReadLine {
     public static void main(String[] args){
-        EditableBufferedReader in = new EditableBufferedReader(new InputStreamReader(System.in));
+        BufferedReader in = new EditableBufferedReader(new InputStreamReader(System.in));
         String str = null;
         try{
             str = in.readLine();
