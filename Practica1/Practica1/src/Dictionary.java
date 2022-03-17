c85e82e2d17beae308517e3e2f9180e0f76a42c1
meritxe11 <90689445+meritxe11@users.noreply.github.com>
2022-03-17 23:09:59 +0100

    Add files via upload

---
+++ Practica1/Practica1/src/Dictionary.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author meritxellfont
+ */
+public class Dictionary {
+    /*
+    public final static int ENTER = 13;//^M
+    public final static int ESC = 27; //^[
+    public final static int INSERT = 45;//^[[2~
+    public final static int SUPR = 46;//^H
+    public final static int RIGHT = 39;//^[[C
+    public final static int LEFT = 37;//^[[D
+    public final static int END = 35;//^[[F
+    public final static int HOME = 36;//^[[H
+    public final static int DELETE = 8;//^[[3~
+    */
+    public final static int CRTL_C = 3;
+    public final static int ENTER = 13;//^M
+    public final static int ESC = 27;//^[
+    public final static int INSERT = 50;//^[[2~
+    public final static int DELETE = 51;//^H
+    public final static int UP = 65;
+    public final static int DOWN = 66;
+    public final static int RIGHT = 67;//^[[C
+    public final static int LEFT = 68;//^[[D
+    public final static int END = 70;//^[[F
+    public final static int HOME = 72;//^[[H
+    public final static int CLAUDATOR = 91;
+    public final static int WAVE = 126;
+    public final static int BACKSPACE = 127;//^[[3~
+
+}
