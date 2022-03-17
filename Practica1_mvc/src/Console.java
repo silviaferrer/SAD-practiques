16b016c51346f2a74b4cd686e5e18613a7f5c49f
meritxe11 <90689445+meritxe11@users.noreply.github.com>
2022-03-18 00:58:40 +0100

    Update Console.java

--- Practica1_mvc/src/Console.java
+++ Practica1_mvc/src/Console.java
@@ -2,10 +2,6 @@
 import java.util.Observable;
 import java.util.Observer;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
 /**
  *
