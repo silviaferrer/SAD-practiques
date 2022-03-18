c340e4ddbf6fa5563442576f58c19e4ad80918bc
meritxe11 <90689445+meritxe11@users.noreply.github.com>
2022-03-18 11:55:48 +0100

    Update Line.java

--- Practica1/Practica1/src/Line.java
+++ Practica1/Practica1/src/Line.java
@@ -4,54 +4,70 @@
  * @author meritxellfont
  */
 public class Line {
+    private StringBuilder linia;
     private int cursor;
-    private StringBuilder stringLine;
-    private boolean insert;
+    private boolean ins;//TRUE: insertChar... FALSE: setChar sobreescriptura
+
+    public StringBuilder getLinia() {
+        return linia;
+    }
 
     public Line() {
-        this.stringLine = new StringBuilder();
+        this.linia = new StringBuilder();
+        this.cursor = 0;
+        this.ins = false;
     }
-    
-    public void addChar(char c){
-        this.stringLine.insert(this.cursor,c);
-        this.cursor++;            
-    }
-    public void home(){
-        this.cursor=0;
-    }
-    public void end(){
-        this.cursor=this.stringLine.length();
-    }
-    public void delete(){
-        if(this.cursor < this.stringLine.length()){     //mirar a veure si hi ha prous caracters per esborrar --> SUPR
-            this.stringLine.deleteCharAt(this.cursor);
+
+    public void addChar(char c) {
+        if (this.ins == true) {
+            //insertar
+            this.linia.setCharAt(cursor, c);
+            this.cursor++;
+        } else {
+            //no insertar
+            this.linia.insert(cursor, c);
+            this.cursor++;
         }
     }
-    public void moveCursor(int move){
-        if(this.cursor + move >= 0 && cursor + move <=this.stringLine.length()){
-            this.cursor+=move;
-        }        
-    }
-    public void backspace(){    //DELETE  <--
-        if(cursor != 0){
-            this.stringLine.deleteCharAt(this.cursor-1);
-            this.cursor--;
+
+    public void insert() {
+        if (this.ins == true) {
+            this.ins = false;
+        } else {
+            this.ins = true;
         }
     }
-    public void insert(){
-        this.insert = !insert;
+
+    public void delete() {//suprimir
+        if (this.cursor < this.linia.length()) {
+            this.linia.deleteCharAt(this.cursor);
+        }
     }
-    public String displayString(){
-        StringBuilder disp = new StringBuilder();
-        disp.append('\r');
-        disp.append(this.stringLine.toString());
-        disp.append(" ");
-        disp.append("\033[");//serveix per moure el cursor?
-        disp.append(1+this.stringLine.length());
-        disp.append("D");
-        return disp.toString();
+
+    public void move(int i) {//rigth ==-1 ; left == 1
+        if (i > 0 && this.cursor != this.linia.length()) {
+            this.cursor += 1;
+        } else if (i < 0 && this.cursor != 0) {
+            this.cursor -= 1;
+        }
     }
-    public String toString(){
-        return this.stringLine.toString();
-    }    
+
+    public void end() {
+        this.cursor = this.linia.length();
+    }
+
+    public void home() {
+        this.cursor = 0;
+    }
+
+    public void backspace() {
+        if (this.cursor != 0) {
+            this.linia.deleteCharAt(this.cursor - 1);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return this.linia.toString();
+    }  
 }
